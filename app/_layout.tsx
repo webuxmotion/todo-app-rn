b184fa9bae715c3860db4df1b00411bffc4cdc79
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2026-01-11 12:44:04 +0200

    index

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,18 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack>
+    <Stack.Screen
+      name="index"
+      options={{
+        title: "Home"
+      }}
+    />
+    <Stack.Screen
+      name="about"
+      options={{
+        title: "About Screen"
+      }}
+    />
+  </Stack>;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,31 @@
-import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.container}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={styles.content}>TODO</Text>
+      <Text>TODO</Text>
+
+      <Text style={styles.content}>hi</Text>
+
+      <Link href={"/about"}>Visit about screen</Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 10,
+  },
+  content: {
+    fontSize: 52,
+    borderWidth: 3,
+    borderColor: "black",
+  }
+})
